2116f81561028384982a98a532b559af3a477ca0
mhdkamran <86851280+mhdkamran@users.noreply.github.com>
2021-09-23 18:59:50 +0530

    Delete Assignment5 directory

--- Assignment5/Question no-6th/index.js
+++
@@ -1,9 +0,0 @@
-const promise1 = Promise.resolve(100);
-const promise2 = 50;
-const promise3 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 80, 'foo');
-});
-
-Promise.all([promise1, promise2, promise3]).then((values) => {
-  console.log(values);
-});

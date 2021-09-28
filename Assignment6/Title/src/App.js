a2e2e4e44b66506b0a06b66ce98cc4cd4d571a86
mhdkamran <86851280+mhdkamran@users.noreply.github.com>
2021-09-29 02:03:00 +0530

    Update App.js

--- Assignment6/Title/src/App.js
+++ Assignment6/Title/src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+const title = "Cognitive Clouds React Training";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-       Cognitive Clouds React Training
-        </a>
-      </header>
+    <div>
+     <h1>{title}</h1>
     </div>
   );
 }
